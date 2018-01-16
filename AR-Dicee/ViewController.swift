842b9aa90fce78db0f2b8df5794bf03083f1fb55
R. Kukuh <rkukuh@gmail.com>
2018-01-16 15:10:47 +0700

    Add moon and earth objects

--- AR-Dicee/ViewController.swift
+++ AR-Dicee/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        createCube()
+        create3DObject()
         
         // Show statistics such as fps and timing information
         //sceneView.showsStatistics = true
@@ -53,23 +53,39 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    func createCube() {
+    func create3DObject() {
         
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        let sunObject = SCNSphere(radius: 0.3)
+        let earthObject = SCNSphere(radius: 0.2)
+        let moonObject = SCNSphere(radius: 0.1)
         
-        let material = SCNMaterial()
+        let sunMaterial = SCNMaterial()
+        let earthMaterial = SCNMaterial()
+        let moonMaterial = SCNMaterial()
         
-        material.diffuse.contents = UIColor.red
+        sunMaterial.diffuse.contents = UIImage(named: "art.scnassets/sun-texture-2k.jpg")
+        earthMaterial.diffuse.contents = UIImage(named: "art.scnassets/earth-texture-2k.jpg")
+        moonMaterial.diffuse.contents = UIImage(named: "art.scnassets/moon-texture-2k.jpg")
         
-        cube.materials = [material]
+        sunObject.materials = [sunMaterial]
+        earthObject.materials = [earthMaterial]
+        moonObject.materials = [moonMaterial]
         
-        let node = SCNNode()
+        let sunNode = SCNNode()
+        let earthNode = SCNNode()
+        let moonNode = SCNNode()
         
-        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        sunNode.position = SCNVector3(x: 0, y: 0.5, z: -0.9)
+        earthNode.position = SCNVector3(x: 0.5, y: 0.3, z: -0.7)
+        moonNode.position = SCNVector3(x: 0.7, y: 0.1, z: -0.5)
         
-        node.geometry = cube
+        sunNode.geometry = sunObject
+        earthNode.geometry = earthObject
+        moonNode.geometry = moonObject
         
-        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.scene.rootNode.addChildNode(sunNode)
+        sceneView.scene.rootNode.addChildNode(earthNode)
+        sceneView.scene.rootNode.addChildNode(moonNode)
         
         sceneView.autoenablesDefaultLighting = true
     }
