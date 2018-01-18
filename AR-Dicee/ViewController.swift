5ce1edd96da41634984a1a62d0afd02698fbf50f
R. Kukuh <rkukuh@gmail.com>
2018-01-18 16:38:22 +0700

    Define touchesBegin() logics

--- AR-Dicee/ViewController.swift
+++ AR-Dicee/ViewController.swift
@@ -88,6 +88,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let touch = touches.first {
+            
+            let touchLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if !results.isEmpty {
+                print("touched the plane")
+            } else {
+                print("touched somewhere else")
+            }
+        }
+    }
+    
     func createPlanetObject() {
         
         let sunObject = SCNSphere(radius: 0.9)
